45bd5648ec40aefb8b9ec4f8dfa69079b9918e41
Pablo <pablogventura@gmail.com>
2020-05-08 12:27:34 -0300

    mas formula aleatoria

--- testing/generadores/formulaaleatoria.py
+++ testing/generadores/formulaaleatoria.py
@@ -4,6 +4,7 @@ simbolos = {"f0":2,"f1":3}
 
 variables = ["x","y","z"]
 
+
 def simbolo_aleatorio():
     sym = choice(list(simbolos.keys()))
     return (sym,simbolos[sym])
@@ -16,6 +17,7 @@ def termino_aleatorio_exacta(p):
     """
     if p == 0:
         return choice(variables)
+    
     s, a = simbolo_aleatorio()
     subterminos = []
     i_exacto = randint(0,a-1)
@@ -25,7 +27,6 @@ def termino_aleatorio_exacta(p):
             subterminos.append(termino_aleatorio_exacta(p-1))
         else:
             subterminos.append(termino_aleatorio_no_exacta(p-1))
-    print(subterminos)
     result=""
     if subterminos:
         result = ", ".join(subterminos)
@@ -47,6 +48,40 @@ def formula_aleatoria(p,f):
     Una formula aleatoria con al menos un termino de profundidad exacta p
     con f cantidad de subformulas
     """
-    
-    for i in range(p):
-        pass
+    result = ""
+    i_exacto = randint(0,f-1)
+    for i in range(f):
+        subformula = ""
+        if i == i_exacto:
+            subformula += termino_aleatorio_exacta(p)
+        else:
+            subformula += termino_aleatorio_no_exacta(p)
+        subformula += " == "
+        subformula += termino_aleatorio_no_exacta(p)
+        subformula = "(%s)" % subformula
+        if randint(0,1) == 1:
+            subformula = "-" + subformula
+        result += subformula
+        if randint(0,1) == 1:
+            result += " | "
+        else:
+            result += " & "
+    result = result[:-3]
+    return result
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
