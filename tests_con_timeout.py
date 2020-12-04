e96bc47c95134c07681ce27ed4107d033bea712a
Pablo Ventura <pablogventura@gmail.com>
2020-12-04 16:50:52 -0300

    test con stderr

--- tests_con_timeout.py
+++ tests_con_timeout.py
@@ -8,13 +8,12 @@ procs = []
 try:
 
     files = [os.path.join(dp, f) for dp, dn, fn in os.walk("testing/") for f in fn]
-    for i,f in enumerate(files):
-        if f.endswith(".model") and not os.path.exists(f.replace(".model",".megahit")):
+    for i, f in enumerate(files):
+        if f.endswith(".model") and not os.path.exists(f.replace(".model", ".megahit")):
             print("%s%%" % (i / len(files)))
             while len(procs) >= cores:
                 procs = [p for p in procs if p.is_running()]
-            procs.append(ShellProc('timeout -s 9 30m python3 main.py "%s" > "%s"' % (f,f.replace(".model",".megahit"))))
-            
+            procs.append(ShellProc('timeout -s 9 10m python3 main.py "%s" > "%s" 2 > "%s"' % (f, f.replace(".model", ".megahit"), f.replace(".model", ".stderr"))))
+
 except KeyboardInterrupt:
     pass
-    
