c8fabdfeff2c5c5d5e0faafc4a0ec5b09010d84f
Pablo Ventura <pablogventura@gmail.com>
2020-12-17 21:25:53 -0300

    preprocesamiento con patrones

--- parser/preprocessing.py
+++ parser/preprocessing.py
@@ -1,7 +1,58 @@
 from collections import defaultdict, OrderedDict
 from first_order import formulas
 from first_order.relops import Relation, Operation
+from misc import indent
 
+
+class Pattern(object):
+    def __init__(self, t):
+        self.tuple = t
+        self.pruned_tuple = []
+        pattern = defaultdict(set)
+        for i, a in enumerate(t):
+            pattern[a].add(i)
+            if len(pattern[a]) == 1:
+                self.pruned_tuple.append(a)
+        self.pruned_tuple = tuple(self.pruned_tuple)
+        self.pattern = frozenset(frozenset(s) for s in pattern.values())
+    
+    def name(self):
+        result = "|"
+        for cls in self.pattern:
+            result += ",".join(str(i) for i in cls)
+            result += "|"
+        result += "a%s" % len(self.pruned_tuple)
+        return result
+    
+    def __hash__(self):
+        return hash(self.pattern)
+    
+    def __eq__(self,other):
+        return hash(self.pattern) == hash(other.pattern)
+    
+    def formula(self):
+        f = formulas.true()
+        vs = formulas.variables(*list(range(len(self.tuple))))
+        differents = []
+        for cls in self.pattern:
+            # aca decimos las partes que son iguales
+            cls = list(cls)
+            for i,j in zip(cls,cls[1:]):
+                f = f & formulas.eq(vs[i], vs[j])
+            # aca viene el momento en que la formula dice que son distintos
+            representante = vs[min(cls)]
+            for otro in differents:
+                f = f & -formulas.eq(representante, otro)
+            differents.append(representante)
+        return f
+    
+    def __repr__(self):
+        result = "Pattern(\n"
+        result += indent("tuple = %s\n" % (self.tuple,))
+        result += indent("pattern = %s\n" % self.pattern)
+        result += indent("formula = %s\n" % self.formula())
+        result += ")"
+        return result
 
 def quotient(s, f):
     # cociente del conjunto s, por la funcion f
@@ -16,15 +67,6 @@ def quotient(s, f):
                 result[a] += result[b]
                 del result[b]
     return result
-
-
-def patron(t):
-    # cociente del conjunto s, por la funcion f
-    result = defaultdict(set)
-    for i, a in enumerate(t):
-        result[a].add(i)
-    return set(frozenset(s) for s in result.values())
-
 
 def limpia(t):
     result = set()
@@ -48,6 +90,7 @@ def formula_patron(t):
     f = formulas.true()
     vs = formulas.variables(*list(range(len(t))))
     tn = tuple(OrderedDict.fromkeys(t))
+    free_vars = tuple(vs[i] for i in (t.index(e) for e in tn))
     for i, a in enumerate(t):
         for j, b in enumerate(t):
             if j <= i:
@@ -56,21 +99,20 @@ def formula_patron(t):
                 f = f & formulas.eq(vs[i], vs[j])
             else:
                 f = f & -formulas.eq(vs[i], vs[j])
-    return f, tn
+    return f, tn, free_vars
 
 
 def preprocesamiento2(target):
-    #target_formula = formulas.RelSym(target.sym,target.arity)(*formulas.variables(*list(range(target.arity))))
+
     pruned_relations = defaultdict(list)
     for t in target.r:
-        formula, pruned_tuple = formula_patron(t)
-        pruned_relations[formula].append(pruned_tuple)
+        pattern = Pattern(t)
+        print(pattern)
+        pruned_relations[pattern].append(pattern.pruned_tuple)
     result = []
-    for formula in pruned_relations:
-        first_tuple = pruned_relations[formula][0]
+    for pattern in pruned_relations:
+        first_tuple = pruned_relations[pattern][0]
         arity = len(first_tuple)
         patron_name = "_".join(str(i) for i in first_tuple)
-        result.append(Relation(target.sym + "a%sp%s" % (arity,patron_name),arity,pruned_relations[formula],formula,target))
-    # return list(fs.values()),list(ts.values())
-    print(result)
+        result.append(Relation(target.sym + pattern.name(),arity,pruned_relations[pattern],pattern.formula(),target))
     return result
