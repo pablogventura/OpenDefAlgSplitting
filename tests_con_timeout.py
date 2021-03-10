108e6c45daf48013af3e299b65614f9d51ffe5f3
Pablo Ventura <pablogventura@gmail.com>
2021-03-10 09:53:33 -0300

    empieza a testear por un orden aleatorio

--- tests_con_timeout.py
+++ tests_con_timeout.py
@@ -2,12 +2,14 @@
 Toma como argumento el directorio donde estan los modelos
 """
 from testing.shell_non_blocking import ShellProc
+from random import shuffle
 import os
 cores = 13
 procs = []
 try:
 
     files = [os.path.join(dp, f) for dp, dn, fn in os.walk("testing/") for f in fn]
+    shuffle(files)
     for i, f in enumerate(files):
         if f.endswith(".model") and not os.path.exists(f.replace(".model", ".megahit")):
             print("%s%%" % (i / len(files)))
