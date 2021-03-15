50be147a8962152be6b9046870bc3cae2140aac0
Pablo Ventura <pablogventura@gmail.com>
2021-03-15 10:44:55 -0300

    debugueando generacion de csv

--- testing/salidas_a_csv.py
+++ testing/salidas_a_csv.py
@@ -1,38 +1,38 @@
-
+import csv
 import os
+import sys
 from pathlib import Path
 
-#https://stackoverflow.com/questions/2084069/create-a-csv-file-with-values-from-a-python-list
+directory = sys.argv[1]
 
+files = [os.path.join(dp, f) for dp, dn, fn in os.walk(directory) for f in fn]
 
-files = [os.path.join(dp, f) for dp, dn, fn in os.walk(".") for f in fn]
+results_file = open("results.csv", 'w')
+wr = csv.writer(results_file, quoting=csv.QUOTE_ALL)
 
-row = []
 for i, f in enumerate(files):
     if f.endswith(".stderr"):
         filename = f
-
+        print(f)
         definable = None
-        nondefinable = None
+        not_definable = None
         timeout = None
         elapsed_time = None
         error = None
-        print(Path(f).stat().st_size)
+        #print(Path(f).stat().st_size)
         if Path(f).stat().st_size > 0:
             
             datafile = open(f,"r")
-            line = datafile.readline()
-            while line:
-                if 'Traceback' in line or 'ERROR' in line:
-                    error = True
+            for line in datafile:
+                if 'Traceback' in line or 'ERROR' in line or "failed" in line:
+                    error = 28
                 if 'NOT DEFINABLE' in line:
+                    print("NODEF")
                     not_definable = True
                 elif 'DEFINABLE' in line:
                     definable = True
                 elif line.startswith("Elapsed time: "):
                     elapsed_time = float(line[len("Elapsed time: "):-1])
-                
-                line = datafile.readline()
             datafile.close()
         else:
             timeout = True
@@ -40,15 +40,15 @@ for i, f in enumerate(files):
         if timeout == True:
             elapsed_time = float('inf')
         else:
-            if definable is None and nondefinable is None:
-                error = True
-            elif definable == nondefinable:
+            if definable is None and not_definable is None:
+                error = 44
+            elif definable == not_definable:
                 ValueError("Inconsistencia")
-            elif nondefinable:
+            elif not_definable:
                 definable = False
             if elapsed_time == None:
-                error = True
+                error = 50
         if error is None:
             error = False
-        
-        print((filename,definable,elapsed_time,error))
+        wr.writerow([filename,definable,elapsed_time,error])
+results_file.close()
