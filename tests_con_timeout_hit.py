5d53363d7afe6300f3329f5569bf5fba22e691f3
Pablo Ventura <pablogventura@gmail.com>
2021-03-25 09:55:47 -0300

    testeo para hit viejo

---
+++ tests_con_timeout_hit.py
@@ -0,0 +1,21 @@
+"""
+Toma como argumento el directorio donde estan los modelos
+"""
+from testing.shell_non_blocking import ShellProc
+from random import shuffle
+import os
+cores = 13
+procs = []
+try:
+
+    files = [os.path.join(dp, f) for dp, dn, fn in os.walk("testing/") for f in fn]
+    shuffle(files)
+    for i, f in enumerate(files):
+        if f.endswith(".model") and not os.path.exists(f.replace(".model", ".hitmegahit")):
+            print("%s%%" % (i / len(files)))
+            while len(procs) >= cores:
+                procs = [p for p in procs if p.is_running()]
+            procs.append(ShellProc('timeout -s 9 30m python3 ../OpenDefAlg/main.py "%s" > "%s" 2 > "%s"' % (f, f.replace(".model", ".hitmegahit"), f.replace(".model", ".hitstderr"))))
+
+except KeyboardInterrupt:
+    pass
